f472e5955abecd4eb0e80a6cd0bc08e0a77a4370
Peter van den Hamer <vdhamer@msn.com>
2022-06-04 19:50:54 +0200

    Initial Commit

---
+++ SwiftLeeAggregator/SwiftLeeAggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLeeAggregatorApp.swift
+//  SwiftLeeAggregator
+//
+//  Created by Peter van den Hamer on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLeeAggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
