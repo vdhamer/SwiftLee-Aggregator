7006e39a3bce77358d547acb6a634e30ff3f5f54
Peter van den Hamer <github@vdhamer.com>
2022-06-21 22:36:09 +0200

    Enabled storing of properties of Feed in CoreData

--- ViewModel/Feed.swift
+++ ViewModel/Feed.swift
@@ -10,20 +10,20 @@ import CoreData
 class Feed: NSManagedObject, Decodable {
 
     enum CodingKeys: String, CodingKey, Hashable { // in same order as JSON file
-//        case url // TODO:
+        case url
         case title
         case urlBase = "link"
-//        case author
-//        case summary = "description"
-//        case imageURL = "image"
+        case author
+        case summary = "description"
+        case imageURL = "image"
     }
 
-//    var url: String = ""
+    var url: String = ""
     var title: String = ""
     var urlBase: String = ""
-//    var author: String = ""
-//    var summary: String = ""
-//    var imageURL: String = "" // var imageURL: URL? { URL(string: imageUrlString) }
+    var author: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
     public var id: String { title }
 
     required convenience init(from decoder: Decoder) throws {
@@ -31,13 +31,11 @@ class Feed: NSManagedObject, Decodable {
         self.init(context: PersistenceController.shared.container.viewContext)
 
         let container = try decoder.container(keyedBy: CodingKeys.self)
-//        self.id = try container.decode(Int64.self, forKey: .id)
-//        self.url = try container.decode(String.self, forKey: .url)
+        self.url = try container.decode(String.self, forKey: .url)
         self.title = try container.decode(String.self, forKey: .title)
         self.urlBase = try container.decode(String.self, forKey: .urlBase)
-//        self.author = try container.decode(String.self, forKey: .author)
-//        self.summary = try container.decode(String.self, forKey: .summary)
-//        self.urlBase = try container.decode(String.self, forKey: .urlBase)
-//        self.imageURL = try container.decode(String.self, forKey: .imageURL)
+        self.author = try container.decode(String.self, forKey: .author)
+        self.summary = try container.decode(String.self, forKey: .summary)
+        self.imageURL = try container.decode(String.self, forKey: .imageURL)
     }
 }
