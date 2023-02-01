f2150ab5ee46dc0c6b503f24784b07cb9e4b2658
Peter van den Hamer <github@vdhamer.com>
2023-02-01 22:07:31 +0100

    Renamed ApiKey file to Rss2jsonApiKey

---
+++ Utilities/Rss2jsonApiKey_.swift
@@ -0,0 +1,8 @@
+//
+//  Rss2jsonApiKey_.swift
+//  SwiftLeeAggregator
+//
+//  Created by Peter van den Hamer on 04/06/2022.
+//
+
+let apiKey = "stah78bcav25ou5pcplmpttfv4bsa2k2ytag1kmd" // for the rss2json.com API. PRIVATE - get your own free key.
